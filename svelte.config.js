8eb9db1d5e121a8941f0143ed96f6920ab22fced
PSJ <astro.psj@gmail.com>
2019-07-29 18:54:00 +0900

    Remove svelte files and change it to HTML5

--- svelte.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint import/no-extraneous-dependencies: off, @typescript-eslint/no-var-requires: off */
-const { preprocess } = require('@pyoner/svelte-ts-preprocess')
-
-module.exports = {
-  preprocess: preprocess(),
-}
